cfa3fcd63559b531edf9bacaae516a47eeaec8ce
Gustavo Cardoso <gusstavocardoso@gmail.com>
2020-10-14 19:45:30 -0300

    First Rest Commit

---
+++ src/test/java/rest/OlaMundoTests.java
@@ -0,0 +1,49 @@
+package rest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class OlaMundoTests {
+    @Test
+    public void responseOlaMundo(){
+        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+        Assert.assertTrue(response.statusCode() == 200);
+        Assert.assertTrue("o status code deveria ser 200",  response.statusCode() == 200);
+        Assert.assertEquals(200,  response.statusCode());
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerOutrasFormasRestAssured(){
+        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerMathersHamcrest(){
+
+    }
+
+    @Test
+    public void devoValidarBody(){
+        given()
+        .when()
+                .get("https://restapi.wcaquino.me/ola")
+        .then()
+                .statusCode(200)
+                .body(is("Ola Mundo!"))
+                .body(containsString("Mundo"))
+                .body(is(not(nullValue())));
+    }
+}
