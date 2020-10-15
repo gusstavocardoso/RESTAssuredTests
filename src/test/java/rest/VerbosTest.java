36e68bc0c3365aa74cdd8a11b4091e51d2417869
Gustavo Cardoso <gusstavocardoso@gmail.com>
2020-10-15 20:30:07 -0300

    Rest Commit 3

--- src/test/java/rest/VerbosTest.java
+++ src/test/java/rest/VerbosTest.java
@@ -36,4 +36,44 @@ public class VerbosTest {
             .body("id", is(nullValue()))
             .body("error", is("Name é um atributo obrigatório"));
     }
+
+    @Test
+    public void deveAlterarUsuario(){
+        given()
+            .log().all()
+            .contentType("application/json")
+            .body("{\"name\":\"Usuario alterado\", \"age\": 80}")
+        .when()
+            .put("https://restapi.wcaquino.me/users/1")
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("id", is(1))
+            .body("name", is("Usuario alterado"))
+            .body("age", is(80))
+            .body("salary", is(1234.5678f));
+    }
+
+    @Test
+    public void deveRemoverUsuario(){
+        given()
+            .log().all()
+        .when()
+            .delete("https://restapi.wcaquino.me/users/1")
+        .then()
+            .log().all()
+            .statusCode(204);
+    }
+
+    @Test
+    public void naoDeveRemoverUsuarioInexistente(){
+        given()
+            .log().all()
+        .when()
+            .delete("https://restapi.wcaquino.me/users/1000 ")
+        .then()
+            .log().all()
+            .statusCode(400)
+            .body("error", is("Registro inexistente"));
+    }
 }
